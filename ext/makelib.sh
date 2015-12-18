d09a12706daef340bde8b9d3d0eb6d928102be6e
tkarrass <tk@bitsetter.de>
2015-12-18 23:18:01 +0000

    Encapsulate some C functions directly

--- ext/makelib.sh
+++ ext/makelib.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 if [ ! -d "ext/wiringPi" ]
 then
-  git clone git://git.drogon.net/wiringPi ext/wiringPi
+  git clone git://git.drogon.net/wiringPi ext/wiringPi > /dev/null
 fi
 
-pushd ext/wiringPi
-git pull
-popd
+pushd ext/wiringPi > /dev/null
+git pull > /dev/null
+popd > /dev/null
 
-make -j5 -C ext/wiringPi/wiringPi static
+make -j5 -C ext/wiringPi/wiringPi static 
 
--- gpigo.go
+++ gpigo.go
@@ -1,43 +1,54 @@
 package main // code.bitsetter.de/tk/gpigo
 //go:generate ./ext/makelib.sh
 
-import (
-	"fmt"
-	"log"
-)
 
 /*
 #cgo CFLAGS: -Iext/wiringPi/wiringPi
 #cgo LDFLAGS: -Lext/wiringPi/wiringPi -lwiringPi
-#include <stdio.h>
 #include "wiringPi.h"
-
-void wpitest() {
-	printf("setup...\n");
-    wiringPiSetup();
-    pinMode(1, OUTPUT);
-	int i;
-    for (i=0; i<10; i++) {
-		digitalWrite(1, HIGH);
-		delay(200);
-		digitalWrite(1, LOW);
-		delay(200);
-	}
-    printf("done.\n");
-}
-
- int sum(int a, int b) {
-   return a+b;
- }
-
 */
 import "C"
 
-func main() {
-	s, err := C.sum(21, 88)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("hi", s )
-	_, err = C.wpitest()
+// void pullUpDnControl(int pin, int pud);
+// void pwmWrite(int pin, int value);
+//   0-1024
+//   not when in Sys mode (wiringPiSetup*)
+//   pin1 (BCM_GPIO18, phys 12)
+// int digitalRead(int pin); HIGH/LOW 1/0
+// analogRead(int pin); // need special board
+// analogWrite(int pin, int value); // need special board
+
+func gpigoInit() (err error) {
+	_, err = C.wiringPiSetup()
+	return
 }
+
+// void pinMode(int pin, int mode);
+func pinMode(pin int, mode int) (err error) {
+	_, err = C.pinMode(C.int(pin), C.int(mode))
+	return
+}
+
+// void digitalWrite(int pin, int value); HIGH, LOW
+func dWrite(pin, value int) error {
+	_, err := C.digitalWrite(C.int(pin), C.int(value))
+	return err
+}
+
+func delay(ms uint) (err error) {
+	_, err = C.delay(C.uint(ms))
+	return
+}
+
+func main() {
+	gpigoInit()
+	pinMode(1, 1) //output
+	for i := 0; i<10; i++ {
+		dWrite(1, 1)
+		delay(100)
+		dWrite(1, 0)
+		delay(300)
+	}
+//	_, err = C.wpitest()
+}
+
